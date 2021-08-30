fda4c2e36e7c6dc9c1415b6ac08ed24777ad1f7f
Beatriz Dantas de Melo <beatrizdantas2312@gmail.com>
2021-08-30 20:34:57 -0300

    soma

--- repeticao.c
+++ repeticao.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 void main(){
-    int i;
+    int x, i, soma = 0;
     for (i = 0; i <10; i++)
     {
-        printf("Condição verdadeira")
+        printf("Digite o item %d:",i);
+        scanf("%d:",&x);
+        soma = soma + x;
     }
+    printf("Soma: %d",soma);
     
 
 }
